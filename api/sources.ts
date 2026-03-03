52a3b66b7338aae53618616daf7ee0211dd63a6e
Ivan Hudko <giv.10072007@gmail.com>
2026-03-03 17:49:27 +0200

    fix versel import problems

--- api/sources.ts
+++ api/sources.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { proxyNewsApiRequest } from './_utils';
+import { proxyNewsApiRequest } from './_utils.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     return proxyNewsApiRequest(req, res, 'top-headlines/sources');
